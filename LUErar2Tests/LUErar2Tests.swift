c8abc4f3351f3ed11a5aa83878f5674f240a6f4d
Mark Larah <mark@mark.local>
2015-02-01 18:01:02 +0000

    Initial Commit

---
+++ LUErar2Tests/LUErar2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  LUErar2Tests.swift
+//  LUErar2Tests
+//
+//  Created by Mark Larah on 01/02/2015.
+//  Copyright (c) 2015 Kalphak. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class LUErar2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
